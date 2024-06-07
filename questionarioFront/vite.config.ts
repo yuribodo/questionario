a3f9f1fda97ecee38f30c5ee070eb71b33f4d3b8
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-06 23:23:21 -0400

    vercel adjustments

--- questionarioFront/vite.config.ts
+++ questionarioFront/vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
-import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          jotaiDebugLabel,
-          jotaiReactRefresh,
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 })
