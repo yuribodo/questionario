dddee7d328550dddd759c910fda0948121932c60
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-06 23:22:24 -0400

    jotai configuration

--- questionarioFront/vite.config.ts
+++ questionarioFront/vite.config.ts
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react'
 import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
 import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          jotaiDebugLabel,
+          jotaiReactRefresh,
+        ],
+      },
+    }),
+  ],
 })
