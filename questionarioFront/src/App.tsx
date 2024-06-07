27d4ac7352807704a9d7f01599a665f013259e53
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-06 23:12:59 -0400

    initial design

--- questionarioFront/src/App.tsx
+++ questionarioFront/src/App.tsx
@@ -4,10 +4,76 @@ function App() {
 
   return (
     <>
-      <div className=''>
-        <p className=''>
-          Teste
-        </p>
+      <div className=' '>
+        <h1 className='  p-4 text-4xl font-bold text-center'>Questionário</h1>
+
+         <div className='flex flex-col'>
+              <div className=' p-3'>
+                <h2 className=' font-semibold text-2xl'>Questao 1</h2>
+                <div className=' bg-blue-400'>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
+                  </p>
+                </div>
+                <div className=' flex flex-col p-3 space-y-2'>
+                  <div className=' bg-green-200 rounded-md '>
+                    <p>opcao1</p>
+                  </div>
+                  <div className=' bg-green-200'>
+                    <p>opcao2</p>
+                  </div>
+                  <div className=' bg-green-200'>
+                    <p>opcao3</p>
+                  </div>
+                </div>  
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+              <div className=' p-3'>
+                <h2 className=' font-semibold text-2xl'>Questao 2</h2>
+                <div className=' bg-blue-400'>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
+                  </p>
+                </div>
+                <div className=' flex flex-col p-3 space-y-2'>
+                  <div className=' bg-green-200 rounded-md '>
+                    <p>opcao1</p>
+                  </div>
+                  <div className=' bg-green-200'>
+                    <p>opcao2</p>
+                  </div>
+                  <div className=' bg-green-200'>
+                    <p>opcao3</p>
+                  </div>
+                </div>  
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+              <div className=' p-3'>
+                <h2 className=' font-semibold text-2xl'>Questao 3</h2>
+                <div className=' bg-blue-400'>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
+                  </p>
+                </div>
+                <div className=' flex flex-col p-3 space-y-2'>
+                  <div className=' bg-green-200 rounded-md '>
+                    <p>opcao1</p>
+                  </div>
+                  <div className=' bg-green-200'>
+                    <p>opcao2</p>
+                  </div>
+                  <div className=' bg-green-200'>
+                    <p>opcao3</p>
+                  </div>
+                </div>  
+            </div>
+          </div>
+
+
       </div>
     </>
   )
