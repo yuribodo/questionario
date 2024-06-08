d661a8244700715ee8f0db4c961a7a79ae38cec4
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-07 21:45:51 -0400

    all questions with the react hook form lib integration

--- questionarioFront/src/App.tsx
+++ questionarioFront/src/App.tsx
@@ -1,83 +1,75 @@
 import './App.css'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 function App() {
-  const {register, handleSubmit} = useForm()
+  const { register, handleSubmit, watch } = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
+  const selectedOption = watch();
 
   return (
     <>
-      <div className=' '>
-        <h1 className='  p-4 text-4xl font-bold text-center'>Questionário</h1>
+      <div className=''>
+        <h1 className='p-4 text-4xl font-bold text-center'>Questionário</h1>
 
-         <div className='flex flex-col'>
-              <div className=' p-3'>
-                <h2 className=' font-semibold text-2xl'>Questao 1</h2>
-                <div className=' bg-blue-400'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
-                  </p>
-                </div>
-                <div className=' flex flex-col p-3 space-y-2'>
-                  <div className=' bg-green-200 rounded-md '>
-                    <p>opcao1</p>
-                  </div>
-                  <div className=' bg-green-200'>
-                    <p>opcao2</p>
-                  </div>
-                  <div className=' bg-green-200'>
-                    <p>opcao3</p>
-                  </div>
-                </div>  
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='flex flex-col'>
+            <div className='p-3'>
+              <h2 className='font-semibold text-2xl'>Questao 1</h2>
+              <div className='bg-blue-400'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
+                </p>
+              </div>
+              <div className='flex flex-col p-3 space-y-2'>
+                {['opcao1', 'opcao2', 'opcao3'].map((option, index) => (
+                  <label key={index} className={`p-2 rounded-md ${selectedOption.questao1 === option ? 'bg-green-400' : 'bg-green-200'}`}>
+                    <input {...register("questao1")} type="radio" value={option} className='hidden' />
+                    {option}
+                  </label>
+                ))}
+              </div>
             </div>
           </div>
 
           <div className='flex flex-col'>
-              <div className=' p-3'>
-                <h2 className=' font-semibold text-2xl'>Questao 2</h2>
-                <div className=' bg-blue-400'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
-                  </p>
-                </div>
-                <div className=' flex flex-col p-3 space-y-2'>
-                  <div className=' bg-green-200 rounded-md '>
-                    <p>opcao1</p>
-                  </div>
-                  <div className=' bg-green-200'>
-                    <p>opcao2</p>
-                  </div>
-                  <div className=' bg-green-200'>
-                    <p>opcao3</p>
-                  </div>
-                </div>  
+            <div className='p-3'>
+              <h2 className='font-semibold text-2xl'>Questao 2</h2>
+              <div className='bg-blue-400'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
+                </p>
+              </div>
+              <div className='flex flex-col p-3 space-y-2'>
+                {['opcao1', 'opcao2', 'opcao3'].map((option, index) => (
+                  <label key={index} className={`p-2 rounded-md ${selectedOption.questao2 === option ? 'bg-green-400' : 'bg-green-200'}`}>
+                    <input {...register("questao2")} type="radio" value={option} className='hidden' />
+                    {option}
+                  </label>
+                ))}
+              </div>
             </div>
           </div>
 
           <div className='flex flex-col'>
-              <div className=' p-3'>
-                <h2 className=' font-semibold text-2xl'>Questao 3</h2>
-                <div className=' bg-blue-400'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
-                  </p>
-                </div>
-                <div className='bg-green-200 p-3 mt-2 rounded-md'>
-                <form onSubmit={handleSubmit((data) => {
-                  console.log(data)
-                })}>
-                    <input {...register("Resposta")} type="text" placeholder='Resposta'  className=' border  border-black w-[1000px]'/>
-                    <div className='flex bg-red-400 rounded-lg w-[80px] justify-center ml-3 p-3 cursor-pointer'>
-                      <input type="submit" className=' cursor-pointer'></input>
-                    </div>
-                  </form>
-                </div> 
+            <div className='p-3'>
+              <h2 className='font-semibold text-2xl'>Questao 3</h2>
+              <div className='bg-blue-400'>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, deserunt, accusamus facilis tempore omnis repudiandae doloribus ipsam debitis corporis dicta facere sequi neque. Soluta eaque nostrum ut hic non eligendi.
+                </p>
+              </div>
+              <div className='bg-green-200 p-3 mt-2 rounded-md'>
+                <input {...register("questao3")} type="text" placeholder='Resposta' className='border border-black w-full' />
+              </div>
             </div>
           </div>
 
-          
-          
-
-
+          <input type="submit" className='bg-red-400 rounded-lg p-2 mt-4 cursor-pointer' value="Submit" />
+        </form>
       </div>
     </>
   )
