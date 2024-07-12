551352291ad89f9574dd1ec42c559cbf37c107c5
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-07-11 21:58:26 -0400

    show answers

--- questionarioFront/src/components/QuestionnaireResponses.tsx
+++ questionarioFront/src/components/QuestionnaireResponses.tsx
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios'; // Você pode usar axios para fazer chamadas à API
-const api = process.env.API_LINK
+import axios from 'axios';
 
+const api = process.env.API_LINK;
 
 interface Response {
   id: string;
   questionId: string;
-  answer: string;
+  resposta: string;
 }
 
 const QuestionnaireResponses: React.FC = () => {
   const { questionnaireId } = useParams<{ questionnaireId: string }>();
   const [responses, setResponses] = useState<Response[]>([]);
-  console.log(responses)
+
   useEffect(() => {
-    // Função para buscar respostas do questionário específico
     const fetchResponses = async () => {
       try {
         const response = await axios.get(`${api}/respostas/questionario/${questionnaireId}`);
-        setResponses(response.data); // Assumindo que a resposta da API retorna um array de respostas
+        setResponses(response.data);
       } catch (error) {
         console.error('Erro ao buscar respostas do questionário:', error);
       }
@@ -32,13 +31,13 @@ const QuestionnaireResponses: React.FC = () => {
     <div>
       <h2 className="text-2xl font-bold mb-4">Respostas do Questionário</h2>
       <div className="grid grid-cols-1 gap-4">
-
-          <div
-            className="p-4 border rounded"
-          >
-                <p>dwadadadad</p>
+        {responses.map((response) => (
+          <div key={response.id} className="p-4 border rounded">
+            <p><strong>Resposta ID:</strong> {response.id}</p>
+            <p><strong>ID da Questão:</strong> {response.questionId}</p>
+            <p><strong>Resposta:</strong> {response.resposta}</p>
           </div>
-    
+        ))}
       </div>
     </div>
   );
